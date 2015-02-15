5e151c8707a0d1bee18656d157161674a470d42f
Hans Guthrie <hansdg1+github@gmail.com>
2015-02-15 16:27:34 -0600

    I think I fixed quantization, but don't fully understand why

--- quantize_image.m
+++ quantize_image.m
@@ -3,16 +3,17 @@ function [ quantized ] = quantize_image( input_img, bits )
 % input_img     : the image to be quantized
 % bits          : the bits/pixel of the output image
 [ m, n ] = size ( input_img );
-figure;
+
 % Should it be 2^(bits-1) ?
 NumLevels = m/2^bits;
 
-Q = 2 / NumLevels; %%%%Not sure about 1 bits/pixel case. 2^1 = 2..? 
+% Q = 2 / NumLevels; %%%%Not sure about 1 bits/pixel case. 2^1 = 2..? 
 
-quantized = sign(input_img) .* floor(input_img / Q) * Q;
+% quantized = sign(input_img) .* floor(input_img / Q) * Q;
+quantized = floor(input_img / NumLevels ) * NumLevels;
 
-imshow(quantized);
-% filename = ['lena_quantized_' num2str(bits) '.bmp'];
-% imwrite(quantized,filename,'bmp');
+% imshow(quantized);
+filename = ['lena_quantized_' num2str(bits) '.bmp'];
+imwrite(quantized,filename,'bmp');
 end
 
